9e97956f33b94d9ac9273bd71a184c4a485ddbf8
Moova Shaka <mrmoovashaka@gmail.com>
2018-06-20 12:44:09 +0100

    prefix /

--- static.config.js
+++ static.config.js
@@ -101,17 +101,17 @@ export default {
             <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico" />
             <title>MoovaShaka || Web Development || WebApp Development || Optimisation || Reactjs || Bespoke Web Design</title>
             {/* Bootstrap Core CSS */}
-            <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+            <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
             {/* Custom Fonts */}
-            <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+            <link href="/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
             <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css" />
             <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic" rel="stylesheet" type="text/css" />
             {/* Plugin CSS */}
-            <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet" />
+            <link href="/vendor/magnific-popup/magnific-popup.css" rel="stylesheet" />
             {/* Theme CSS */}
-            <link href="css/creative.min.css" rel="stylesheet" />
+            <link href="/css/creative.min.css" rel="stylesheet" />
             {/* Additional CSS goes here */}
-            <link href="css/form.min.css" rel="stylesheet" />
+            <link href="/css/form.min.css" rel="stylesheet" />
           </Head>
           <Body>
             {children}
@@ -134,10 +134,10 @@ export default {
               </select>
               <textarea name="message" />
             </form>
-            <script src="vendor/jquery/jquery.min.js" />
-            <script src="js/creative.min.js" />
-            <script src="js/jquery.easing.min.js" />
-            <script src="vendor/bootstrap/js/bootstrap.min.js" />
+            <script src="/vendor/jquery/jquery.min.js" />
+            <script src="/js/creative.min.js" />
+            <script src="/js/jquery.easing.min.js" />
+            <script src="/vendor/bootstrap/js/bootstrap.min.js" />
           </Body>
         </Html>
       )
